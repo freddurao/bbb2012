71968384b45203c26987f08f80550e86f4bdf9e5
freddurao <freddurao@gmail.com>
2012-07-23 22:36:17 -0300

    reggae

--- my_project/test_scripts/v_user.py
+++ my_project/test_scripts/v_user.py
@@ -1,6 +1,7 @@
 
 import random
 import time
+import requests
 
 
 class Transaction(object):
@@ -8,12 +9,12 @@ class Transaction(object):
         pass
 
     def run(self):
-        r = random.uniform(1, 2)
-        time.sleep(r)
-        self.custom_timers['Example_Timer'] = r
-
+        #r = random.uniform(1, 2)
+        #time.sleep(r)
+        #self.custom_timers['Example_Timer'] = r
+        r = requests.get('http://localhost:8000/polls/1/')
+        r.raw.read()          
 
 if __name__ == '__main__':
     trans = Transaction()
     trans.run()
-    print trans.custom_timers
